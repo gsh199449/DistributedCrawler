5fd618c5247694b1937136381ab81f4f78686d4d
gsh199449 <gsh199449@hotmail.com>
2014-01-06 20:29:34 +0800

    建立了SolrIndex

--- src/main/java/com/gs/indexer/solr/SolrIndex.java
+++ src/main/java/com/gs/indexer/solr/SolrIndex.java
@@ -1,4 +1,31 @@
 package com.gs.indexer.solr;
 
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.common.SolrInputDocument;
+
+import com.gs.model.PagePOJO;
+
+
 public class SolrIndex {
+	public static final void index(final Set<PagePOJO> set,
+			final String SERVER_URL) throws SolrServerException, IOException {
+		SolrServer server = new HttpSolrServer(SERVER_URL);
+		Set<SolrInputDocument> result = new HashSet<SolrInputDocument>();
+		for (PagePOJO pojo : set) {
+			SolrInputDocument doc;
+			doc = new SolrInputDocument();
+			doc.addField("id", pojo.getId());
+			doc.addField("title", pojo.getTitle());
+			doc.addField("content", pojo.getContent());
+			result.add(doc);
+		}
+		server.add(result);
+		server.commit();
+	}
 }

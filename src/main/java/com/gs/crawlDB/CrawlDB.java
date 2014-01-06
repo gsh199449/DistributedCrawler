03a82496ae895c3f1046fb81874b98f908eb85eb
“gsh199449” <gsh199449@hotmail.com>
2014-01-06 21:54:40 +0800

    Redis test success

--- src/main/java/com/gs/crawlDB/CrawlDB.java
+++ src/main/java/com/gs/crawlDB/CrawlDB.java
@@ -2,13 +2,21 @@ package com.gs.crawlDB;
 
 import java.util.Set;
 
+import javax.swing.JEditorPane;
+
+import org.apache.commons.pool.impl.GenericObjectPool.Config;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
 import com.gs.crawler.Redis;
 import com.gs.utils.URL;
 
 public class CrawlDB {
-	private Redis redis = Redis.getInstance();
+	private static final JedisPool tocrawl = new JedisPool(new Config(),"localhost",6377,1000,"940409",0);
+	private static final JedisPool crawled = new JedisPool(new Config(),"localhost",6377,1000,"940409",1);
 	public Set<URL> generate() {
-
+		//TODO:tocrawl.getResource().randomKey()
 		return null;
 	}
 
---
+++ src/test/java/com/gs/crawldb/TestJedis.java
@@ -0,0 +1,16 @@
+package com.gs.crawldb;
+
+import org.apache.commons.pool.impl.GenericObjectPool.Config;
+import org.junit.Test;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+public class TestJedis {
+	@Test
+	public void test(){
+		JedisPool j = new JedisPool(new Config(),"localhost",6377,1000,"940409",1);
+		System.out.println(j.getResource().get("aa"));
+		j.getResource().set("java", "haha");
+	}
+}

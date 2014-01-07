b38da7b1ba6959e0c20beee0ddfe1650601ebdc0
gsh199449 <gsh199449@hotmail.com>
2014-01-07 15:44:00 +0800

    将ID从String改为Int

--- src/main/java/com/gs/indexer/solr/SolrSearcher.java
+++ src/main/java/com/gs/indexer/solr/SolrSearcher.java
@@ -35,7 +35,7 @@ public class SolrSearcher {
 		SolrDocumentList docs = response.getResults();
 		Set<PagePOJO> result = new HashSet<PagePOJO>();
 		for (SolrDocument doc : docs) {
-			String id = (String) doc.getFieldValue("id");
+			int id = (int) doc.getFieldValue("id");
 			result.add(dao.loadPage(id));
 		}
 		return result;

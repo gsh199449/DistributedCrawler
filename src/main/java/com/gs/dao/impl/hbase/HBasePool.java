cba062c266f0b5bf04abaefc6af03d52a416cf5b
“gsh199449” <gsh199449@hotmail.com>
2014-01-08 21:29:03 +0800

    add a getConf method

--- src/main/java/com/gs/dao/impl/hbase/HBasePool.java
+++ src/main/java/com/gs/dao/impl/hbase/HBasePool.java
@@ -3,23 +3,26 @@ package com.gs.dao.impl.hbase;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.ZooKeeperConnectionException;
 import org.apache.hadoop.hbase.client.HConnection;
 import org.apache.hadoop.hbase.client.HConnectionManager;
 import org.apache.hadoop.hbase.client.HTableInterface;
 
-import com.gs.dao.impl.mysql.DbcpBean;
-
 public final class HBasePool {
 	private static HBasePool instance = null;
 	private static HConnection connection = null;
+	private static Configuration conf;
+
+	public Configuration getConf() {
+		return conf;
+	}
 
 	private HBasePool() throws ZooKeeperConnectionException {
 		Configuration conf = new Configuration();
 		conf.set("hbase.zookeeper.property.clientPort", "2181");
 		conf.set("hbase.zookeeper.quorum", "gs-pc");
 		conf.set("hbase.master", "gs-pc:60000");
+		this.conf = conf;
 		connection = HConnectionManager.createConnection(conf);
 	}
 

08689689a6ec28d9385a40ff8c824db965511bd0
“gsh199449” <gsh199449@hotmail.com>
2014-01-09 16:55:10 +0800

    disable hbasecleaner

--- src/main/java/com/gs/dao/impl/hbase/HBaseCleaner.java
+++ src/main/java/com/gs/dao/impl/hbase/HBaseCleaner.java
@@ -4,23 +4,22 @@ import java.io.IOException;
 
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
-import org.apache.hadoop.hbase.ZooKeeperConnectionException;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
 
 public final class HBaseCleaner {
-	public static void main(String[] args) throws ZooKeeperConnectionException, IOException {
-		clean();
-	}
-	
-	private static void clean() throws IOException{
+	public static void clean(String tableName) throws IOException{
 		HBaseAdmin admin = new HBaseAdmin(HBasePool.getInstance().getConf());
-		admin.disableTable("page");
-		admin.deleteTable("page");
-		HTableDescriptor tableDescriptor = new HTableDescriptor("page");  
+		admin.disableTable(tableName);
+		admin.deleteTable(tableName);
+		HTableDescriptor tableDescriptor = new HTableDescriptor(tableName);  
         tableDescriptor.addFamily(new HColumnDescriptor("content"));  
         tableDescriptor.addFamily(new HColumnDescriptor("title"));  
         tableDescriptor.addFamily(new HColumnDescriptor("url"));  
 		admin.createTable(tableDescriptor);
 		admin.close();
+	}
+	
+	public static void main(String[] args) throws IOException {
+		clean("page");
 	}
 }

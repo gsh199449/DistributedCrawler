64e6c54830c7dd545bfd57c4b3d796668d960e28
gsh199449 <gsh199449@hotmail.com>
2014-01-09 15:33:01 +0800

    添加了ConfigurationReader和conf文件夹

---
+++ src/main/java/com/gs/main/ConfigurationReader.java
@@ -0,0 +1,85 @@
+package com.gs.main;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationFactory.ConfigurationBuilder;
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.JsonToken;
+import org.codehaus.jackson.map.JsonMappingException;
+
+public class ConfigurationReader {
+	public static Configuration parseJsonConf(String path) throws IOException {
+		ConfigurationBuilder builder = new ConfigurationBuilder();
+		Configuration conf = null;
+			conf = builder.getConfiguration();
+		try {
+			JsonFactory jfactory = new JsonFactory();
+			JsonParser jParser = jfactory.createJsonParser(new File(
+					path));
+			while (jParser.nextToken() != JsonToken.END_OBJECT) {
+				String fieldname = jParser.getCurrentName();
+				if ("rootPath".equals(fieldname)) {
+					jParser.nextToken();
+					conf.addProperty("rootPath", jParser.getText());
+				} else if ("seeds".equals(fieldname)) {
+					jParser.nextToken();
+					conf.addProperty("seeds", jParser.getText());
+				} else if ("depth".equals(fieldname)) {
+					jParser.nextToken();
+					conf.addProperty("depth", jParser.getIntValue());
+				} else if ("topN".equals(fieldname)) {
+					jParser.nextToken();
+					conf.addProperty("topN", jParser.getIntValue());
+				} else if ("outputPath".equals(fieldname)) {
+					jParser.nextToken();
+					conf.addProperty("outputPath", jParser.getText());
+				} else if ("jobName".equals(fieldname)) {
+					jParser.nextToken();
+					conf.addProperty("jobName", jParser.getText());
+				} else if ("crawlDBHost".equals(fieldname)) {
+					jParser.nextToken();
+					conf.addProperty("crawlDBHost", jParser.getText());
+				} else if ("crawlDBPort".equals(fieldname)) {
+					jParser.nextToken();
+					conf.addProperty("crawlDBPort", jParser.getIntValue());
+				} else if ("crawlDBPassword".equals(fieldname)) {
+					jParser.nextToken();
+					conf.addProperty("crawlDBPassword", jParser.getText());
+				} else if ("crawlDBTimeout".equals(fieldname)) {
+					jParser.nextToken();
+					conf.addProperty("crawlDBTimeout", jParser.getIntValue());
+				} else if ("crawlDBToCrawlDB".equals(fieldname)) {
+					jParser.nextToken();
+					conf.addProperty("crawlDBToCrawlDB", jParser.getIntValue());
+				} else if ("crawlDBCrawledDB".equals(fieldname)) {
+					jParser.nextToken();
+					conf.addProperty("crawlDBCrawledDB", jParser.getIntValue());
+				} else if ("solrURL".equals(fieldname)) {
+					jParser.nextToken();
+					conf.addProperty("solrURL", jParser.getText());
+				} else if ("jarsName".equals(fieldname)) {
+					jParser.nextToken();
+					List<String> jarlist = new LinkedList<String>();
+					while (jParser.nextToken() != JsonToken.END_ARRAY) {
+						jarlist.add(jParser.getText());
+					}
+					conf.addProperty("jarsName", jarlist);
+				}
+			}
+			jParser.close();
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return conf;
+	}
+}
--- src/main/java/com/gs/main/MainClass.java
+++ src/main/java/com/gs/main/MainClass.java
@@ -27,20 +27,22 @@ import com.gs.indexer.solr.SolrIndex;
  * @author gaoshen
  */
 public class MainClass {
-	private static final String rootPath = "hdfs://gs-pc:9000/home/test/";
-	private static final String dst = rootPath + "qq.txt";// 首页的链接暂存地
-	private static final int depth = 3;// 深度
-	private static final int topN = 100;// 每页抓取的最大链接数
-	private static final String outputPath = rootPath + "output";// 最终结果的输出路径
-	private static final String jobName = "DistributeCrawler";// Job的名称
-	private static final String crawlDBHost = "gs-pc";//CrawlDB的IP
-	private static final int crawlDBPost = 6377;//CrawlDB的端口
-	private static final String crawlDBPassword = "940409";//CrawlDB的密码
-	private static final int crawlDBTimeout = 1000;//CrawlDB的连接超时时间
-	private static final int crawlDBToCrawlDB = 0;//CrawlDB的待抓取的数据库编号
-	private static final int crawlDBCrawledDB = 1;//CrawlDB的已抓取的数据库编号
-	private static final String SolrURL = "http://gs-pc:8888/solr";//Solr服务器URL
-	private static final Logger LOG = LoggerFactory.getLogger(MainClass.class);
+	private static String rootPath = "hdfs://gs-pc:9000/home/test/";
+	private static String seeds = rootPath + "qq.txt";// 首页的链接暂存地
+	private static int depth = 3;// 深度
+	private static int topN = 100;// 每页抓取的最大链接数
+	private static String outputPath = rootPath + "output";// 最终结果的输出路径
+	private static String jobName = "DistributeCrawler";// Job的名称
+	private static String crawlDBHost = "gs-pc";//CrawlDB的IP
+	private static int crawlDBPort = 6377;//CrawlDB的端口
+	private static String crawlDBPassword = "940409";//CrawlDB的密码
+	private static int crawlDBTimeout = 1000;//CrawlDB的连接超时时间
+	private static int crawlDBToCrawlDB = 0;//CrawlDB的待抓取的数据库编号
+	private static int crawlDBCrawledDB = 1;//CrawlDB的已抓取的数据库编号
+	private static String solrURL = "http://gs-pc:8888/solr";//Solr服务器URL
+	private static String libsPath = "hdfs://gs-pc:9000/home/test/libs/";//Solr服务器URL
+	private static String[] jarsName = {"gson-2.2.4.jar"};//Solr服务器URL
+	private static Logger LOG = LoggerFactory.getLogger(MainClass.class);
 
 	/**
 	 * Mapper类
@@ -52,11 +54,11 @@ public class MainClass {
 
 		public void map(LongWritable key, Text value, Context context)
 				throws IOException, InterruptedException {
-			CrawlDB db = new CrawlDB(crawlDBHost, crawlDBPost, crawlDBPassword, crawlDBTimeout, crawlDBToCrawlDB, crawlDBCrawledDB);
+			CrawlDB db = new CrawlDB(crawlDBHost, crawlDBPort, crawlDBPassword, crawlDBTimeout, crawlDBToCrawlDB, crawlDBCrawledDB);
 			Crawler c = new Crawler(key.toString(), value.toString(), topN,
 					depth, db, context);// 以Input文件的行偏移量作为crawler的id
 			try {
-				SolrIndex.index(c.start(), SolrURL);
+				SolrIndex.index(c.start(), solrURL);
 			} catch (SolrServerException e) {
 				LOG.error(e.getMessage());
 				e.printStackTrace();
@@ -67,31 +69,40 @@ public class MainClass {
 
 	public static void main(String[] args) throws Exception {
 		long startTime = System.currentTimeMillis();
+		org.apache.commons.configuration.Configuration conf = ConfigurationReader.parseJsonConf("conf/json.json");
+		rootPath = conf.getString("rootPath");
+		seeds = conf.getString("seeds");
+		depth = conf.getInt("depth");
+		topN = conf.getInt("topN");
+		outputPath = conf.getString("outputPath");
+		jobName = conf.getString("jobName");
+		crawlDBHost = conf.getString("crawlDBHost");
+		crawlDBPort = conf.getInt("crawlDBPort");
+		crawlDBPassword = conf.getString("crawlDBPassword");
+		crawlDBTimeout = conf.getInt("crawlDBTimeout");
+		crawlDBToCrawlDB = conf.getInt("crawlDBToCrawlDB");
+		crawlDBCrawledDB = conf.getInt("crawlDBCrawledDB");
+		solrURL = conf.getString("solrURL");
+		libsPath = conf.getString("libsPath");
+		jarsName = conf.getStringArray("jarsName");
 		LOG.info("Job init "+jobName+" start time : "+new SimpleDateFormat("yyyy-mm-dd HH:mm:ss").format(new Date(startTime)));
-		Configuration conf = new Configuration();
-		FileSystem fs = FileSystem.get(conf);
+		Configuration hadoopConf = new Configuration();
+		FileSystem fs = FileSystem.get(hadoopConf);
 		if (fs.exists(new Path(outputPath))) {// 如果输出路径存在的话，删除他。
 			LOG.info("Delete the output path" + outputPath);
 			fs.delete(new Path(outputPath), true);
 		}
-		ExternalJARAdder adder = new ExternalJARAdder(fs, conf);
-		adder.add(rootPath + "libs/gson-2.2.4.jar");
-		adder.add(rootPath + "libs/solr-solrj-4.0.0.jar");
-		adder.add(rootPath + "libs/commons-dbcp-1.4.jar");
-		adder.add(rootPath + "libs/commons-pool-1.5.4.jar");
-		adder.add(rootPath + "libs/httpclient-4.1.3.jar");
-		adder.add(rootPath + "libs/httpcore-4.1.4.jar");
-		adder.add(rootPath + "libs/httpmime-4.1.3.jar");
-		adder.add(rootPath + "libs/hbase-0.94.11.jar");
-		adder.add(rootPath + "libs/zookeeper-3.4.5.jar");
-		adder.add(rootPath + "libs/protobuf-java-2.4.0a.jar");
-		adder.add(rootPath + "libs/jedis-2.1.0.jar");
-		Job job = new Job(conf, jobName);
+		ExternalJARAdder adder = new ExternalJARAdder(fs, hadoopConf);
+		if(!libsPath.endsWith("/")){libsPath = libsPath + "/";}
+		for(String jar : jarsName){
+			adder.add(libsPath + jar);
+		}
+		Job job = new Job(hadoopConf, jobName);
 		job.setJarByClass(MainClass.class);
 		job.setMapperClass(CrawlMapper.class);
 		job.setOutputKeyClass(NullWritable.class);
 		job.setOutputValueClass(Text.class);
-		FileInputFormat.addInputPath(job, new Path(dst));
+		FileInputFormat.addInputPath(job, new Path(seeds));
 		FileOutputFormat.setOutputPath(job, new Path(outputPath));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 		LOG.info("Job finish . Use : "+(startTime-System.currentTimeMillis()));
